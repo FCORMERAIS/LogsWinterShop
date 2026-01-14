43e4801519108a43620ed5a52259ed18517170f4
Flav <flaviocormerais3@gmail.com>
2026-01-14 14:18:00 +0100

    changement silver_logs.sqlé

--- models/silver/silver_logs.sql
+++ models/silver/silver_logs.sql
@@ -1,42 +1,38 @@
 WITH parsed AS (
     SELECT
-        log_line,
+        -- Identifiant et date d'intégration depuis la table Bronze
+        id AS bronze_id,
+        ingested_at,
  
         -- IP
         split_part(log_line, ' ', 1) AS ip_address,
  
-        -- Date
+        -- Date du log
         to_timestamp(
             regexp_replace(
-                regexp_substr(log_line, '\\[(.*?)\\]'), 
-                '\\[|\\]', ''
-            ), 
+                split_part(log_line, '[', 2),
+                '\\].*', ''
+            ),
             'DD/Mon/YYYY:HH24:MI:SS'
         ) AS log_date,
  
         -- Méthode HTTP
-        regexp_substr(log_line, '"(GET|POST|PUT|DELETE|PATCH)') AS http_method,
+        split_part(split_part(log_line, '"', 2), ' ', 1) AS http_method,
  
-        -- URL
-        regexp_substr(log_line, '"(?:GET|POST|PUT|DELETE|PATCH) (.*?) HTTP/') AS url_path,
+        -- URL demandée
+        split_part(split_part(log_line, '"', 2), ' ', 2) AS url_path,
  
         -- Code HTTP
-        split_part(
-            regexp_substr(log_line, '" (\\d{3}) '), 
-            ' ', 2
-        )::int AS status_code,
+        split_part(split_part(log_line, '"', 3), ' ', 2)::int AS status_code,
  
         -- Taille de la réponse
-        split_part(
-            regexp_substr(log_line, '" \\d{3} (\\d+)'), 
-            ' ', 2
-        )::int AS response_size,
+        split_part(split_part(log_line, '"', 3), ' ', 3)::int AS response_size,
  
         -- Referrer
-        regexp_substr(log_line, '"https?://[^"]*"', 1, 2) AS referrer,
+        split_part(split_part(log_line, '"', 4), '"', 1) AS referrer,
  
         -- User agent
-        regexp_substr(log_line, '".*"$') AS user_agent
+        split_part(split_part(log_line, '"', 6), '"', 1) AS user_agent
  
     FROM {{ source('prod', 'bronze_logs') }}
 )
